00545adad42f1e94653c8f47fe684a4323fff374
Tim <tim.morriss@gmail.com>
2020-12-18 10:49:53 +0000

    day5 part 1

---
+++ Day 5/day_5.py
@@ -0,0 +1,39 @@
+from utility.read_file import ReadFile
+
+
+def upper_lower(num_range, instruction):
+    instruction = instruction.replace('L', 'F')
+    instruction = instruction.replace('R', 'B')
+    row_final = 0
+    for char in instruction:
+        diff = num_range[1] - num_range[0]
+        if diff == 1:
+            if char == 'F' and num_range[0] > row_final:
+                row_final = num_range[0]
+            elif char == 'B' and num_range[1] > row_final:
+                row_final = num_range[1]
+        elif char == 'F':
+            num_range = (num_range[0], num_range[0] + round(diff / 2) - 1)
+            # print("F! row number: %s " % str(num_range))
+        elif char == 'B':
+            num_range = (num_range[1] - round(diff / 2) + 1, num_range[1])
+            # print("B! row number: %s " % str(num_range))
+    return row_final
+
+
+def day_5_part_one():
+    total = 0
+    for line in ReadFile.read_lines('day_5_input.txt'):
+        row = line[:-4]
+        row_final = upper_lower((0, 127), row)
+        # print(row_final)
+        column = line[-4:]
+        column_final = upper_lower((0, 7), column)
+        # print(column_final)
+        product = (row_final * 8) + column_final
+        if product > total:
+            total = product
+    return total
+
+
+print(day_5_part_one())
